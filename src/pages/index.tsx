2ba301a26196052f80565c79c63adf19f677090a
Rafael Moura <rafaelmoura2929@gmail.com>
2020-07-08 16:48:26 -0300

    Adding editorconfig and typescript

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,10 @@
-import Head from 'next/head'
+import Head from "next/head";
 
-export default function Home() {
+type Props = {
+  title: string;
+};
+
+export default function Home({ title = "React avançado" }: Props) {
   return (
     <div className="container">
       <Head>
@@ -10,7 +14,7 @@ export default function Home() {
 
       <main>
         <h1 className="title">
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
+         {title}
         </h1>
 
         <p className="description">
@@ -54,7 +58,7 @@ export default function Home() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Powered by{' '}
+          Powered by{" "}
           <img src="/vercel.svg" alt="Vercel Logo" className="logo" />
         </a>
       </footer>
@@ -205,5 +209,5 @@ export default function Home() {
         }
       `}</style>
     </div>
-  )
+  );
 }
