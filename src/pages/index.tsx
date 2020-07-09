951c950652bf481b751afa355c0fe406030f72a8
Rafael Moura <rafaelmoura2929@gmail.com>
2020-07-09 16:07:49 -0300

    Adding eslint and prettier

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,21 +1,19 @@
-import Head from "next/head";
+import Head from 'next/head'
 
 type Props = {
-  title: string;
-};
+  title: string
+}
 
-export default function Home({ title = "React avançado" }: Props) {
+export default function Home({ title = 'React Avançado!' }: Props) {
   return (
     <div className="container">
       <Head>
-        <title>Create Next App</title>
+        <title>{title}</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
       <main>
-        <h1 className="title">
-         {title}
-        </h1>
+        <h1 className="title">{title}</h1>
 
         <p className="description">
           Get started by editing <code>pages/index.js</code>
@@ -58,7 +56,7 @@ export default function Home({ title = "React avançado" }: Props) {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Powered by{" "}
+          Powered by{' '}
           <img src="/vercel.svg" alt="Vercel Logo" className="logo" />
         </a>
       </footer>
@@ -72,7 +70,6 @@ export default function Home({ title = "React avançado" }: Props) {
           justify-content: center;
           align-items: center;
         }
-
         main {
           padding: 5rem 0;
           flex: 1;
@@ -81,7 +78,6 @@ export default function Home({ title = "React avançado" }: Props) {
           justify-content: center;
           align-items: center;
         }
-
         footer {
           width: 100%;
           height: 100px;
@@ -90,49 +86,40 @@ export default function Home({ title = "React avançado" }: Props) {
           justify-content: center;
           align-items: center;
         }
-
         footer img {
           margin-left: 0.5rem;
         }
-
         footer a {
           display: flex;
           justify-content: center;
           align-items: center;
         }
-
         a {
           color: inherit;
           text-decoration: none;
         }
-
         .title a {
           color: #0070f3;
           text-decoration: none;
         }
-
         .title a:hover,
         .title a:focus,
         .title a:active {
           text-decoration: underline;
         }
-
         .title {
           margin: 0;
           line-height: 1.15;
           font-size: 4rem;
         }
-
         .title,
         .description {
           text-align: center;
         }
-
         .description {
           line-height: 1.5;
           font-size: 1.5rem;
         }
-
         code {
           background: #fafafa;
           border-radius: 5px;
@@ -141,17 +128,14 @@ export default function Home({ title = "React avançado" }: Props) {
           font-family: Menlo, Monaco, Lucida Console, Liberation Mono,
             DejaVu Sans Mono, Bitstream Vera Sans Mono, Courier New, monospace;
         }
-
         .grid {
           display: flex;
           align-items: center;
           justify-content: center;
           flex-wrap: wrap;
-
           max-width: 800px;
           margin-top: 3rem;
         }
-
         .card {
           margin: 1rem;
           flex-basis: 45%;
@@ -163,29 +147,24 @@ export default function Home({ title = "React avançado" }: Props) {
           border-radius: 10px;
           transition: color 0.15s ease, border-color 0.15s ease;
         }
-
         .card:hover,
         .card:focus,
         .card:active {
           color: #0070f3;
           border-color: #0070f3;
         }
-
         .card h3 {
           margin: 0 0 1rem 0;
           font-size: 1.5rem;
         }
-
         .card p {
           margin: 0;
           font-size: 1.25rem;
           line-height: 1.5;
         }
-
         .logo {
           height: 1em;
         }
-
         @media (max-width: 600px) {
           .grid {
             width: 100%;
@@ -203,11 +182,10 @@ export default function Home({ title = "React avançado" }: Props) {
             Oxygen, Ubuntu, Cantarell, Fira Sans, Droid Sans, Helvetica Neue,
             sans-serif;
         }
-
         * {
           box-sizing: border-box;
         }
       `}</style>
     </div>
-  );
+  )
 }
